b8f65fb7a45ba7adfc21a955ed54aa31ff7e7016
fiatjaf <fiatjaf@gmail.com>
2017-11-02 22:26:39 +0000

    make elm-package happy with documentation.

--- src/Hashbow.elm
+++ src/Hashbow.elm
@@ -1,5 +1,15 @@
 module Hashbow exposing (hashbow, color, toHex)
 
+{-| Generate hexadecimal (usable as colors) hashes from arbitrary strings.
+
+The hashes are not collision-resistant at all, but they are useful, for
+example, if you have to generate a custom different color for each element
+of a collection of whatever you may have in your app.
+
+@docs hashbow, color, toHex
+
+-}
+
 import Color exposing (Color)
 import List exposing (..)
 import String
@@ -7,10 +17,27 @@ import Hex
 import Char.CodePoint
 
 
+{-| The simplest method: takes any String and outputs a color in hex format.
+
+    hashbow "Bruce Willis" == "#bf408e"
+    hashbow "Nicolas Cage" == "#40bf8c"
+
+-}
 hashbow : String -> String
 hashbow = color 0.5 0.5 >> toHex
 
 
+{-| By default, `hashbow` uses the HSL colors scheme to generate the hashes,
+with the hue provided by the string and saturation and lightness set to 0.5
+each. Here you can generate a Color with different saturation and lightness.
+
+    color 0.5 0.5 "Bruce Willis" == "#bf408e"
+
+`hashbow` is defined like this:
+
+    hashbow = color 0.5 0.5 >> toHex
+
+-}
 color : Float -> Float -> String -> Color
 color saturation lightness str =
   let
@@ -21,6 +48,12 @@ color saturation lightness str =
   in
     Color.hsl hue saturation lightness
 
+{-| Takes a Color and turns it into a hex format. Just a helper for when you
+use `color` instead of `hashbow`.
+
+    color 0.5 0.5 "Bruce Willis" |> toHex == "#bf408e"
+
+-}
 toHex : Color -> String
 toHex color =
   let
